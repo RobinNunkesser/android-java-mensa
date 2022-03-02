b3c815a391dd1946809338a6c0db1a49543ed12e
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-03-02 09:53:32 +0100

    Bottom navigation

--- app/src/main/java/de/hshl/isd/mensa/MainActivity.java
+++ app/src/main/java/de/hshl/isd/mensa/MainActivity.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import de.hshl.isd.mensa.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavController mNavController;
+    private BottomNavigationView mNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
-        }
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment_container);
+        mNavView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(mNavView, mNavController);
     }
 }
